23fa27b580d9632159810cab14697d3e17fb2e35
jinhu <jinhu@LAPTOP-PDBLC4SB>
2020-02-07 15:02:11 +0800

    1

--- Test1/src/com/tedu/Test2.java
+++ Test1/src/com/tedu/Test2.java
@@ -1,1 +1,10 @@
 //111111111111111111
+package com.tedu;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println(1111);
+		System.out.println(2222);
+		//111111111111111
+	}
+}
